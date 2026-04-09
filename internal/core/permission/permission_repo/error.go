78780c9cade444a4cf44e2072b1185af00195b7f
Housi <chousiadas6395@gmail.com>
2026-04-09 22:54:57 +0300

    remove repos from core directory

--- internal/core/permission/permission_repo/error.go
+++
@@ -1,5 +0,0 @@
-package permission_repo
-
-import "errors"
-
-var errOrderFieldNotFound = errors.New("order field not found")
--- internal/core/user_roles_permissions/user_roles_permissions_repo/error.go
+++
@@ -1,5 +0,0 @@
-package user_roles_permissions_repo
-
-import "errors"
-
-var errOrderFieldNotFound = errors.New("order field not found")
